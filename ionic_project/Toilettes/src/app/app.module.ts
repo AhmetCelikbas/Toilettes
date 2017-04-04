1a0c6c9620318a760fae517a66e831dc594a13d5
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-04-04 18:02:55 +0200

    Update, tabs and tabs titles

--- ionic_project/Toilettes/src/app/app.module.ts
+++ ionic_project/Toilettes/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { AddPage } from '../pages/add/add';
+import { ToilettesPage } from '../pages/toilettes/toilettes';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -13,8 +13,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
+    AddPage,
+    ToilettesPage,
     TabsPage
   ],
   imports: [
@@ -24,8 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
+    AddPage,
+    ToilettesPage,
     TabsPage
   ],
   providers: [
