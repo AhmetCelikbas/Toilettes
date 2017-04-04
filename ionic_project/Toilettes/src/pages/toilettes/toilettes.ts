b972241dd949fe97312b3e98a40a427939ddb58e
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-04-04 18:56:34 +0200

    Add import Ionic native and rename app title and add imports of Google Maps Native

--- ionic_project/Toilettes/src/pages/toilettes/toilettes.ts
+++ ionic_project/Toilettes/src/pages/toilettes/toilettes.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import {
+ GoogleMap,
+ GoogleMapsEvent,
+ GoogleMapsLatLng,
+ CameraPosition,
+ GoogleMapsMarkerOptions,
+ GoogleMapsMarker
+} from 'ionic-native';
 
 @Component({
   selector: 'page-toilettes',
   templateUrl: 'toilettes.html'
 })
+
 export class ToilettesPage {
+  private map: GoogleMap;
+  private geolocationOptions: any;
+  private userPosition: GoogleMapsLatLng;
+  private checkinPosition: GoogleMapsLatLng;
+  private cameraPos: CameraPosition
+
 
   constructor(public navCtrl: NavController) {
 
   }
+
+
+
+  
 
 }
