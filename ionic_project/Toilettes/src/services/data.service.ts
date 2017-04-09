06bea8df47c74d0d0b4c55cd37dfdf9abe1c7ef0
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-04-09 21:25:36 +0200

    Update data service
    add token from local storage, handle authentication error

--- ionic_project/Toilettes/src/services/data.service.ts
+++ ionic_project/Toilettes/src/services/data.service.ts
@@ -1,12 +1,14 @@
+import {AlertController} from 'ionic-angular';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import { ConfigService } from './config.service';
+
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
 
-  constructor( private config: ConfigService, public http: Http) {
+  constructor( private config: ConfigService, public http: Http, public alertCtrl: AlertController) {
     console.log('Hello DataService Provider');
   }
 
@@ -23,6 +25,39 @@ export class DataService {
       res => res.json()
     )
   }
+
+  public post(verb:string, data) {
+    let headers = new Headers({
+      'Content-Type': 'application/json',
+      'authorization': localStorage.getItem('token')
+     });
+    return new Promise(resolve => {
+      this.http.post(this.config.apiUrl + verb, data, {headers: headers}).subscribe(
+        data => {
+            if(data.json()) {
+              resolve(data.json());
+            } else {
+              resolve(false);
+            }
+        },
+        err => {
+          if(err.status == 403) {
+            console.log(JSON.parse(err._body)['message'])
+            let alert = this.alertCtrl.create({
+              title: 'Connection requise',
+              subTitle: 'Vous devez vous connecter pour effectuer cette action',
+              buttons: ['OK']
+            });
+            alert.present().then(
+              () => {
+                resolve(false);
+              })
+          }
+        }
+      )
+    });
+  }
+
 
   // public getCityNameFromLatLng(lat, lng) {
   //   return this.http.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=" + this.config.GoogleMapsApiKey).map ( 
