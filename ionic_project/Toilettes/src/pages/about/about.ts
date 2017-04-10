98709a0c0eef6a65cef776c62745d2f8a79e2d82
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-04-10 11:52:09 +0200

    add signin signup button on about page

--- ionic_project/Toilettes/src/pages/about/about.ts
+++ ionic_project/Toilettes/src/pages/about/about.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import {PopoverController, NavController} from 'ionic-angular';
+
+import { SigninSignupPopoverPage } from '../signin-signup-popover/signin-signup-popover';
+
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  private token : string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(  
+    public navCtrl: NavController,
+    public popoverCtrl: PopoverController
+    ) {
+  }
+
+  getToken(){
+    return window.localStorage.getItem('token');
+  }
+
+  logout(){
+    window.localStorage.removeItem('token');
+    this.token = null;
+  }
+
+  login(){
+
+    //popover login
+    let popover = this.popoverCtrl.create(SigninSignupPopoverPage);
+      // popover.present({ev: event});
+      popover.present({ev: event});
 
   }
+
 
 }
