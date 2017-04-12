dcf1d571dc8258dabaa19c7f34cf77c17d44b8eb
younes038 <younes038@users.noreply.github.com>
2017-04-12 16:42:35 +0200

    Update model details

--- nodejs_api/models/details.js
+++ nodejs_api/models/details.js
@@ -1,26 +1,27 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-	var Details = sequelize.define('Details', {
-		id_toilet: DataTypes.BIGINT,
-		name: DataTypes.STRING,
-		access: DataTypes.BOOLEAN,
-		exist: DataTypes.BOOLEAN,
-		rating: DataTypes.INTEGER,
-		fee: DataTypes.BOOLEAN,
-		male: DataTypes.BOOLEAN,
-		female: DataTypes.BOOLEAN,
-		wheelchair: DataTypes.BOOLEAN,
-		drinking_water: DataTypes.BOOLEAN,
-		placeType: DataTypes.STRING,
-		createdAt: DataTypes.DATE,
-   		updatedAt: DataTypes.DATE
-	}, {
-		classMethods: {
-			associate: function(models) {
-				// associations can be defined here
-				Details.belongsTo(models.Toilet, { foreignKey: "id" });
-			}
-		}
-	});
-	return Details;
+  var Details = sequelize.define('Details', {
+    id_toilet: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    access: DataTypes.BOOLEAN,
+    exist: DataTypes.BOOLEAN,
+    fee: DataTypes.BOOLEAN,
+    male: DataTypes.BOOLEAN,
+    female: DataTypes.BOOLEAN,
+    wheelchair: DataTypes.BOOLEAN,
+    drinking_water: DataTypes.BOOLEAN,
+    place_type: {
+			type:   DataTypes.ENUM,
+			values: ['restaurant', 'public', 'shopping center', 'gas station', 'bar']
+		},
+    address: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+        Details.belongsTo(models.Toilet, {foreignKey: 'id_toilet'});
+      }
+    }
+  });
+  return Details;
 };
