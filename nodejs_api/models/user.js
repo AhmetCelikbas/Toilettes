262b41bc43aaa7e70865fe8af424d8a2f79979d3
younes038 <younes038@users.noreply.github.com>
2017-04-12 16:41:09 +0200

    Update model user

--- nodejs_api/models/user.js
+++ nodejs_api/models/user.js
@@ -3,15 +3,13 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
+    password: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        User.hasMany(models.Toilet, {foreignKey: 'id_user', as: 'Toilet' });
-        User.hasMany(models.Comments, {foreignKey: 'id_user', as: 'Comments' });
+        User.hasMany(models.Toilet, { foreignKey: 'id_user' });
+        User.hasMany(models.Comment, { foreignKey: 'id_user' });
       }
     }
   });
